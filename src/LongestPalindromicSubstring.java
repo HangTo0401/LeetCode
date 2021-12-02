19e25ca321dc2d41c51331a5399a57cd050746cd
HangTo <tominhhang1996@gmail.com>
2021-12-02 08:20:17 +0700

    Add solutions to those problems

--- src/LongestPalindromicSubstring.java
+++ src/LongestPalindromicSubstring.java
@@ -4,7 +4,9 @@ public class LongestPalindromicSubstring {
      * MEDIUM
      * 5. Given a string s, return the longest palindromic substring in s.
      *
-     * SOLUTION: Create resultStart with the begin index of the longest substring
+     * SOLUTION: There are 3 solutions to this problem
+     *
+     * C3. Create resultStart with the begin index of the longest substring
      * resultLength is length of the longest substring
      * Iterate through each character and find the middle point of string s
      * Then we expand from middle point on both sides to find the longest palindrome substring
@@ -28,15 +30,68 @@ public class LongestPalindromicSubstring {
      * Input: s = "ac"
      * Output: "a"
      * */
+    // Approach 1: Brute Force
+    public String longestPalindromeSubstringUsingBruteForce(String s) {
+        if (s.length() <= 1) return s;
+
+        String longestPalindromeSubstring = "";
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                String substr = s.substring(i, j+1);
+                if (isPalindrome(substr) && substr.length() > longestPalindromeSubstring.length()) {
+                    // Update longestPalindromeSubstring
+                    longestPalindromeSubstring = substr;
+                }
+            }
+        }
+
+        return longestPalindromeSubstring;
+    }
+
+    public boolean isPalindrome(String s) {
+        StringBuilder stringBuilder = new StringBuilder(s);
+        String reversed = stringBuilder.reverse().toString();
+        return reversed.equals(s);
+    }
+
+    // Approach 2: Dynamic Programming
+    public String longestPalindromeUsingDynamicProgramming(String s) {
+        if(s == null || s.length() <= 1 ) return s;
+
+        int len = s.length();
+
+        boolean[][] dp = new boolean[len][len];
+
+        int startMax = 0, maxLen = 1;
+        for(int end = 0; end < s.length(); end++) {
+            for(int start = 0; start <= end; start++) {
+                if(s.charAt(start) == s.charAt(end)) {
+                    if (end - start > 2) {
+                        dp[start][end] = dp[start + 1][end - 1];
+                    } else {
+                        dp[start][end] =  true;
+                    }
+                }
+
+                if(dp[start][end] == true && end - start + 1 > maxLen) {
+                    maxLen = end - start + 1;
+                    startMax = start;
+                }
+            }
+        }
+
+        return s.substring(startMax, startMax + maxLen);
+    }
+
     private int  resultStart = 0;
     private int  resultLength = 0;
-
+    // Approach 3: Expand Around Center
     // This function is used to expand substring on both sides
     public void expandRange(String s, int begin, int end) {
         while (begin >=0 && end < s.length() && s.charAt(begin) == s.charAt(end)) {
             // Expand range on both sides
-           begin--;
-           end++;
+            begin--;
+            end++;
         }
 
         // Check if any substring has longer than resultLength then update
@@ -51,7 +106,7 @@ public class LongestPalindromicSubstring {
         }
     }
 
-    public String longestPalindrome(String s) {
+    public String longestPalindromeSubstringUsingExpandAroundCenter(String s) {
         if (s.length() < 2) return s;
 
         for (int start = 0; start < s.length(); start++) {
@@ -64,6 +119,14 @@ public class LongestPalindromicSubstring {
 
     public static void main(String[] args) {
         LongestPalindromicSubstring longestPalindromicSubstring = new LongestPalindromicSubstring();
-        System.out.println(longestPalindromicSubstring.longestPalindrome("babad"));
+
+        // C1
+        System.out.println(longestPalindromicSubstring.longestPalindromeSubstringUsingBruteForce("babad"));
+
+        // C2
+        System.out.println(longestPalindromicSubstring.longestPalindromeUsingDynamicProgramming("babad"));
+
+        // C3
+        System.out.println(longestPalindromicSubstring.longestPalindromeSubstringUsingExpandAroundCenter("babad"));
     }
 }
--- src/LongestSubstringWithoutRepeatingCharacters.java
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,4 +1,8 @@
+import javafx.scene.Parent;
+
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class LongestSubstringWithoutRepeatingCharacters {
 
@@ -7,7 +11,9 @@ public class LongestSubstringWithoutRepeatingCharacters {
      * 3. Longest Substring Without Repeating Characters
      * Given a string s, find the length of the longest substring without repeating characters.
      *
-     * SOLUTION: Create Hashset and variable maxLength to store length of max substring
+     * SOLUTION: There are 3 solutions to this problem
+     *
+     * Create Hashset and variable maxLength to store length of max substring
      * Create index i and j
      * Then iterate through all characters in input s and check if hashset contain any character at index i
      * If it contains, set maxLength as max(size of hashset, maxLength) and increase index i
@@ -50,8 +56,83 @@ public class LongestSubstringWithoutRepeatingCharacters {
         return maxLengthOfSubstring;
     }
 
+    // Approach 1: Brute Force
+    public int lengthOfLongestSubstringBruteForce(String s) {
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                if (checkDuplicate(s, i, j)) {
+                    result = Math.max(result, j-i+1);
+                }
+            }
+        }
+        return result;
+    }
+
+    public boolean checkDuplicate(String s, int start, int end) {
+        int[] chars = new int[128];
+
+        for (int i = start; i <= end; i++) {
+            char c = s.charAt(i);
+            chars[c]++;
+            if (chars[c] > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Approach 2: Sliding Window
+    public int lengthOfLongestSubstringUsingSlidingWindow(String s) {
+        int[] chars = new int[128];
+
+        int left = 0;
+        int right = 0;
+
+        int res = 0;
+        while (right < s.length()) {
+            char r = s.charAt(right);
+            chars[r]++;
+
+            while (chars[r] > 1) {
+                char l = s.charAt(left);
+                chars[l]--;
+                left++;
+            }
+
+            res = Math.max(res, right - left + 1);
+
+            right++;
+        }
+        return res;
+    }
+
+    // Approach 3: Sliding Window Optimized
+    public int lengthOfLongestSubstringUsingHashmap(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)) + 1, i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         LongestSubstringWithoutRepeatingCharacters longestSubstring = new LongestSubstringWithoutRepeatingCharacters();
         System.out.println(longestSubstring.lengthOfLongestSubstring("abcaecbb"));
+
+        // C1
+        System.out.println(longestSubstring.lengthOfLongestSubstringBruteForce("abcaecbb"));
+
+        // C2
+        System.out.println(longestSubstring.lengthOfLongestSubstringUsingSlidingWindow("abcaecbb"));
+
+        // C3
+        System.out.println(longestSubstring.lengthOfLongestSubstringUsingHashmap("abcaecbb"));
     }
 }
