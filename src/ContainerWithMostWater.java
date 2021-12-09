39c8bd576dd5ddd831fcac871008aa21b838e62a
HangTo <tominhhang1996@gmail.com>
2021-12-09 09:04:30 +0700

    Implement 11. Container With Most Water

--- src/ContainerWithMostWater.java
+++ src/ContainerWithMostWater.java
@@ -10,7 +10,8 @@ public class ContainerWithMostWater {
      * Example 1:
      * Input: height = [1,8,6,2,5,4,8,3,7]
      * Output: 49
-     * Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+     * Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7].
+     * In this case, the max area of water (blue section) the container can contain is 49.
      *
      * Example 2:
      * Input: height = [1,1]
@@ -29,7 +30,64 @@ public class ContainerWithMostWater {
      * 2 <= n <= 105
      * 0 <= height[i] <= 104
      * */
-    public static void main(String[] args) {
 
+    /**
+     * Approach 1: BruteForce using nested loop
+     * @param height
+     * @return int
+     * */
+    public int maxAreaBruteForce(int[] height) {
+        int maxArea = 0;
+
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i+1; j < height.length; j++) {
+                maxArea = Math.max(maxArea, Math.min(height[i], height[j]) * (j-i));
+            }
+        }
+        return maxArea;
+    }
+
+    /**
+     * Approach 2: Two Pointer Approach
+     *
+     * Algorithm
+     * The intuition behind this approach is that the area formed between the lines will always be limited by the height of the shorter line.
+     * Further, the farther the lines, the more will be the area obtained.
+     *
+     * We take two pointers, one at the beginning and one at the end of the array constituting the length of the lines.
+     * Futher, we maintain a variable maxarea to store the maximum area obtained till now.
+     *
+     * At every step, we find out the area formed between them,
+     * update maxarea and move the pointer pointing to the shorter line towards the other end by one step.
+     * The algorithm can be better understood by looking at the example below:
+     * @param height
+     * @return int
+     * */
+    public int maxAreaUsingTwoPointers(int[] height) {
+        int maxArea = 0;
+        int begin = 0;
+        int end = height.length - 1;
+
+        while(begin < end) {
+            maxArea = Math.max(maxArea, Math.min(height[begin], height[end]) * (end - begin));
+            if (height[begin] < height[end]) {
+                begin++;
+            } else {
+                end--;
+            }
+        }
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        ContainerWithMostWater solution = new ContainerWithMostWater();
+
+        int[] height = new int[]{1,8,6,2,5,4,8,3,7};
+
+        // C1
+        System.out.println(solution.maxAreaBruteForce(height));
+
+        // C2
+        System.out.println(solution.maxAreaUsingTwoPointers(height));
     }
 }
