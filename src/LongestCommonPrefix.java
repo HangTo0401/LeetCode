fbf9579ff8680353ab2e85957a19d366926b3597
HangTo <tominhhang1996@gmail.com>
2021-12-13 08:08:17 +0700

    Implement 14. longest common prefix

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -19,6 +19,7 @@ public class LongestCommonPrefix {
      * 1 <= strs.length <= 200
      * 0 <= strs[i].length <= 200
      * strs[i] consists of only lower-case English letters.
+     *
      * Accepted
      * 1,313,856
      * Submissions
@@ -33,11 +34,18 @@ public class LongestCommonPrefix {
      * , finding at each iteration ii the longest common prefix of strings LCP(S1…Si)
      * When LCP(S1…Si) is an empty string, the algorithm ends.
      * Otherwise, after nn iterations, the algorithm returns LCP(S1…Sn).
+     *
+     * Complexity Analysis
+     * Time complexity : O(S), where S is the sum of all characters in all strings.
+     * In the worst case all nn strings are the same. The algorithm compares the string S1 with the other strings S2…Sn
+     * There are SS character comparisons, where SS is the sum of all characters in the input array.
+     * Space complexity : O(1). We only used constant extra space.
+     *
      * @param strs
      * @return String
      * */
-    public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) return "";
+    public String longestCommonPrefixUsingHorizontalScanning(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
 
         String prefix = strs[0];
         for (int i = 1; i < strs.length; i++) {
@@ -49,11 +57,48 @@ public class LongestCommonPrefix {
         return prefix;
     }
 
+    /**
+     * Approach 2: Vertical scanning
+     *
+     * Algorithm
+     * Imagine a very short string is the common prefix at the end of the array. The above approach will still do SS comparisons.
+     * One way to optimize this case is to do vertical scanning.
+     * We compare characters from top to bottom on the same column (same character index of the strings) before moving on to the next column.
+     *
+     * Complexity Analysis
+     * Time complexity : O(S), where S is the sum of all characters in all strings.
+     * In the worst case there will be nn equal strings with length mm and the algorithm performs S=m*n character comparisons.
+     * Even though the worst case is still the same as Approach 1, in the best case there are at most n*minLen comparisons
+     * where minLen is the length of the shortest string in the array.
+     * Space complexity : O(1). We only used constant extra space.
+     *
+     * @param strs
+     * @return String
+     * */
+    public String longestCommonPrefixUsingVerticalScanning(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (c != strs[j].charAt(i)) {
+                    return strs[0].substring(0, i);
+                } else if (i == strs[i].length()) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+
     public static void main(String[] args) {
         LongestCommonPrefix solution = new LongestCommonPrefix();
         String[] strs = new String[]{"flower","flow","flight"};
 
         // C1
-        System.out.println(solution.longestCommonPrefix(strs));
+        System.out.println(solution.longestCommonPrefixUsingHorizontalScanning(strs));
+
+        // C2
+        System.out.println(solution.longestCommonPrefixUsingVerticalScanning(strs));
     }
 }
