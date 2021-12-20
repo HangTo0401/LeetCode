db1bb2979b2174c1c99d9ec36f76e73f8166cd8c
HangTo <tominhhang1996@gmail.com>
2021-12-20 21:34:51 +0700

    Implement ThreeSum

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -1,4 +1,5 @@
-import java.util.List;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class ThreeSum {
     /**
@@ -37,13 +38,87 @@ public class ThreeSum {
      * */
 
     /**
-     * 
+     * Approach 1: Using Hashset and Two pointers
+     * @param nums
      * */
     public List<List<Integer>> threeSum(int[] nums) {
+        if(nums.length==0 || nums==null) return new ArrayList<>();
 
+        Arrays.sort(nums);
+        HashSet set = new HashSet();
+
+        for (int i = 0; i < nums.length; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    set.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                }
+                if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        List<List<Integer>> list = new ArrayList<List<Integer>>(set);
+        return list;
     }
+
+    /**
+     * Approach 2: Using TwoSum method
+     * Time complexity is still O(N^2)
+     * @param nums
+     * */
+    public static List<List<Integer>> threeSumUsingTwoSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (nums.length < 3) return res;
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            ArrayList<List<Integer>> twoSumList = twoSum(nums, i, 0 - nums[i]);
+            for (List<Integer> ls : twoSumList) {
+                ls.add(nums[i]);
+                res.add(ls);
+            }
+        }
+
+        return res;
+    }
+
+    public static ArrayList<List<Integer>> twoSum(int[] nums, int index, int target) {
+        HashMap<Integer, Integer> map = new HashMap();
+        int count = 0;
+        HashSet<ArrayList<Integer>> set = new HashSet();
+
+        for (int i = index + 1; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                ArrayList<Integer> cur = new ArrayList<>();
+                cur.add(target - nums[i]);
+                cur.add(nums[i]);
+                Collections.sort(cur);
+                set.add(cur);
+                break;
+            }
+            map.put(nums[i], count++);
+        }
+
+        ArrayList<List<Integer>> result = new ArrayList<List<Integer>>(set);
+        return result;
+    }
+
     public static void main(String[] args) {
         ThreeSum solution = new ThreeSum();
+        int[] nums1 = new int[]{-1,0,1,2,-1,-4};
 
+        // C1
+        solution.threeSum(nums1);
+
+        // C2
+        solution.threeSumUsingTwoSum(nums1);
     }
 }
