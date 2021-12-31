1b9bad3181baa63c631185f292610f1f9e4a6e6d
HangTo <tominhhang1996@gmail.com>
2021-12-31 13:25:50 +0700

    Implement ValidParentheses

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -1,2 +1,60 @@
+import java.util.Stack;
+
 public class ValidParentheses {
+    /**
+     * EASY
+     * 20. Valid Parentheses
+     * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+     * determine if the input string is valid.
+     * An input string is valid if:
+     * Open brackets must be closed by the same type of brackets.
+     * Open brackets must be closed in the correct order.
+     *
+     * Example 1:
+     * Input: s = "()"
+     * Output: true
+     *
+     * Example 2:
+     * Input: s = "()[]{}"
+     * Output: true
+     *
+     * Example 3:
+     * Input: s = "(]"
+     * Output: false
+     *
+     * Constraints:
+     * 1 <= s.length <= 104
+     * s consists of parentheses only '()[]{}'.
+     * */
+    public boolean isValid(String s) {
+        if (s.length() == 1) return false;
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(0);
+            } else if (s.charAt(i) == '{') {
+                stack.push(1);
+            } else if (s.charAt(i) == '[') {
+                stack.push(2);
+            } else if (stack.size() == 0) {
+                return false;
+            } else if (stack.pop() != 0 && s.charAt(i) == ')') {
+                return false;
+            } else if (stack.pop() != 1 && s.charAt(i) == '}') {
+                return false;
+            } else if (stack.pop() != 2 && s.charAt(i) == ']') {
+                return false;
+            }
+        }
+
+        if(stack.size() !=0) return false;
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidParentheses solution = new ValidParentheses();
+        System.out.println(solution.isValid("({[)}"));
+    }
 }
